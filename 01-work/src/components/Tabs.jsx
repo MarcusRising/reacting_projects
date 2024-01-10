27d8b75fefdd01c6fe5968445fc241d1585ad480
MarcusRising <puck408@gmail.com>
2024-01-10 12:16:49 -0700

    fixed a usability issue
    Tabs component is easier to use now that the ButtonsContainer is default to menu.
    But can be easily changed in future and can be set to whatever is needed

--- 01-work/src/components/Tabs.jsx
+++ 01-work/src/components/Tabs.jsx
@@ -1,5 +1,5 @@
-export default function Tabs({ children, buttons, ButtonsContainer }) {
-  //const ButtonsContainer = buttonsContainer;
+export default function Tabs({ children, buttons, ButtonsContainer = "menu" }) {
+  //ButtonContainer defaults to "menu"
   return (
     <>
       <ButtonsContainer>{buttons}</ButtonsContainer>
