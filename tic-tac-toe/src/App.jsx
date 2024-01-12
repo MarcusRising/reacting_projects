7a231f775c5e4d73739bf5c4d2482430c3bf1da4
MarcusRising <puck408@gmail.com>
2024-01-12 09:22:33 -0700

    winner function

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
 
     gameBoard[row][col] = player;
   }
-
+  let winner = null;
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol =
       gameBoard[combination[0].row][combination[0].column];
@@ -38,6 +38,13 @@ function App() {
       gameBoard[combination[1].row][combination[1].column];
     const thirdSquareSymbol =
       gameBoard[combination[2].row][combination[2].column];
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
   }
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -68,6 +75,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
+        {winner && <p>You won, {winner}</p>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
