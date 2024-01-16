f5d080d19bc86fe9caa0b40771116e5f33e0fb1f
MarcusRising <puck408@gmail.com>
2024-01-16 14:03:28 -0700

    Inputs added

--- Investment_Calculator/src/App.jsx
+++ Investment_Calculator/src/App.jsx
@@ -1,7 +1,14 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput />
+      <Results />
+    </>
+  );
 }
 
-export default App
+export default App;
